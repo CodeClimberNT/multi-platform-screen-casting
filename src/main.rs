bbb21255bdb79d1c1394c8de42053161e324f36e
Ale240x <alessia.bevilacqua24@gmail.com>
2024-09-26 18:20:24 +0200

    prima bozza di UI

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,65 @@
 use capture_screen::{capture_screen};
+use eframe::{egui, App, run_native};
 
 mod capture_screen;
 
+    
 
 fn main() {
-    capture_screen();
+    // Crea una finestra nativa
+    let options = eframe::NativeOptions::default();
+    run_native(
+        "rustream",  // Titolo della finestra
+        options,
+        Box::new(|_cc| Ok(Box::new(MyApp::default())))
+    );
 }
+
+// Definisci la tua struttura MyApp
+#[derive(Default)]
+struct MyApp {
+    button1_clicked: bool,
+    button2_clicked: bool,
+}
+
+impl App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // Creiamo un pannello centrale
+        egui::CentralPanel::default().show(ctx, |ui| {
+
+            ui.horizontal_centered(|ui| {
+                
+
+                ui.vertical_centered( |ui| {
+                    
+                    ui.add_space(40.0);
+                    ui.heading("rustream");
+                    ui.add_space(40.0);
+                    
+                    // Primo bottone
+                    if ui.button("Cast New Streaming").clicked() {
+                        self.button1_clicked = true;
+                        self.button2_clicked = false;
+                    }
+                    
+                    ui.add_space(20.0);
+    
+                    // Secondo bottone
+                    if ui.button("View Streaming").clicked() {
+                        self.button2_clicked = true;
+                        self.button1_clicked = false;
+                    }
+        
+                    // Mostra il messaggio relativo al bottone cliccato
+                    if self.button1_clicked {
+                        ui.label("Hai cliccato Cast New Streaming");
+                    } else if self.button2_clicked {
+                        ui.label("Hai cliccato View Existing Streaming");
+                    }
+                });
+            });
+  
+        });
+    }
+}
+
